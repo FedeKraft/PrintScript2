34b0be4674b7da1cb6b3d770ea4845b3db1a207a
Your GitHub Username <your.email@example.com>
2024-08-27 13:33:39 -0700

    probando pre-commit

---
+++ linter/src/main/kotlin/rules/CamelCaseIdentifierRule.kt
@@ -0,0 +1,17 @@
+package org.example.rules
+
+import ASTNode
+import IdentifierNode
+import org.example.LinterError
+
+class CamelCaseIdentifierRule : LinterRule {
+    override fun check(node: ASTNode): List<LinterError> {
+        val errors = mutableListOf<LinterError>()
+        if (node is IdentifierNode) {
+            if (!node.name.matches(Regex("^[a-z]+([A-Z][a-z]*)*$"))) {
+                errors.add(LinterError("Identifier ${node.name} should be in camel case", node.line, node.column))
+            }
+        }
+        return errors
+    }
+}
