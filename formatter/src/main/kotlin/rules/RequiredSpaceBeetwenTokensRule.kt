eab93ad6cfb3dc55835e065725d356ecc08e70c4
JeroEngel <85593415+JeroEngel@users.noreply.github.com>
2024-08-28 18:37:18 -0300

    prueba

--- formatter/src/main/kotlin/rules/RequiredSpaceBeetwenTokensRule.kt
+++ formatter/src/main/kotlin/rules/RequiredSpaceBeetwenTokensRule.kt
@@ -5,6 +5,7 @@ import ASTNode
 class RequiredSpaceBeetwenTokensRule : FormatterRule {
     override fun apply(node: ASTNode, code: StringBuilder): StringBuilder {
         TODO("Not yet implemented")
+
     }
 
 }
