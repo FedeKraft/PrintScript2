9c89ace4a7d755d5b6b297cc0ad3f3bc6d3e0fdb
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-09-02 21:03:44 -0300

    factories y tests lexer, parser, interpreter

--- lexer/src/test/kotlin/LexerTest3.kt
+++ lexer/src/test/kotlin/LexerTest3.kt
@@ -1,9 +1,10 @@
 import lexer.LexerFactory
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import token.Token
 import token.TokenType
-import token.TokenValue
 import java.io.File
 
 class LexerTest3 {
@@ -16,24 +17,83 @@ class LexerTest3 {
     fun `test full code with types and operators`() {
         val code = readSourceCodeFromFile("AllKnownTokens.txt")
         val lexer = LexerFactory().createLexer1_0(code)
-        val currentStatementTokens = mutableListOf<Token>() // Lista temporal para almacenar tokens de la sentencia actual
+        val actualTokens = mutableListOf<Token>()
 
         while (lexer.hasNextToken()) {
             val token = lexer.nextToken()
-            currentStatementTokens.add(token)
+            actualTokens.add(token)
+        }
+        println(actualTokens)
+    }
 
-            if (token.type == TokenType.SEMICOLON) {
-                // Imprime los tokens de la sentencia actual
-                println(currentStatementTokens)
-                // Limpia la lista para la próxima sentencia
-                currentStatementTokens.clear()
+    @Test
+    fun `test lexer partial lines`() {
+        val code = readSourceCodeFromFile("AllKnownTokens.txt")
+        val lexer = LexerFactory().createLexer1_0(code)
+
+        // Function to lex a specified number of lines
+        fun lexLines(numberOfLines: Int): List<Token> {
+            val tokens = mutableListOf<Token>()
+            var linesLexed = 0
+
+            while (lexer.hasNextToken() && linesLexed < numberOfLines) {
+                val token = lexer.nextToken()
+                tokens.add(token)
+                if (token.type == TokenType.SEMICOLON) {
+                    linesLexed++
+                }
+            }
+            return tokens
+        }
+
+        // Lex the first 2 lines
+        val tokensFirstTwoLines = lexLines(3)
+
+        println("Tokens from the first three lines: $tokensFirstTwoLines")
+        assertEquals(19, tokensFirstTwoLines.size) // Adjust the expected number according to your expectations
+    }
+
+    @Test
+    fun `test lexer with no errors`() {
+        val code = readSourceCodeFromFile("AllKnownTokens.txt")
+        val lexer = LexerFactory().createLexer1_0(code)
+
+        val tokensBeforeError = mutableListOf<Token>()
+        var errorEncountered = false
+
+        while (lexer.hasNextToken() && !errorEncountered) {
+            val token = lexer.nextToken()
+            tokensBeforeError.add(token)
+
+            if (token.type == TokenType.UNKNOWN) {
+                errorEncountered = true
+                println("Error encountered: $token")
             }
         }
 
-        // Imprime cualquier token restante que no haya terminado con un ';'
-        if (currentStatementTokens.isNotEmpty()) {
-            println(currentStatementTokens)
-        }
+        assertFalse(errorEncountered, "Expected an error token but none was found")
+        println("Tokens before error: $tokensBeforeError")
     }
 
+    @Test
+    fun `test lexer stops at errors`() {
+        val code = readSourceCodeFromFile("StatementsWithErrors.txt")
+        val lexer = LexerFactory().createLexer1_0(code)
+
+        val tokensBeforeError = mutableListOf<Token>()
+        var errorEncountered = false
+
+        while (lexer.hasNextToken() && !errorEncountered) {
+            val token = lexer.nextToken()
+            tokensBeforeError.add(token)
+
+            if (token.type == TokenType.UNKNOWN) {
+                errorEncountered = true
+                println("Error encountered: $token")
+            }
+        }
+
+        assertTrue(errorEncountered, "Expected an error token but none was found")
+        println("Tokens before error: $tokensBeforeError")
+    }
 }
--- parser/src/test/kotlin/ParserTest.kt
+++ parser/src/test/kotlin/ParserTest.kt
@@ -1,46 +1,73 @@
-import command.ParseCommand
-import command.VariableDeclarationStatementCommand
 import lexer.LexerFactory
-import org.example.command.AssignationCommand
-import org.example.parser.Parser
 import org.junit.jupiter.api.Test
-import token.TokenType
+import parser.ParserFactory
 import java.io.File
 
-class ParserTest{
+class ParserTest {
 
     private fun readSourceCodeFromFile(filename: String): String {
         return File("src/test/resources/$filename").readText()
     }
+
     @Test
-    fun testParse(){
+    fun testParse() {
         val lexer = LexerFactory().createLexer1_0(readSourceCodeFromFile("testCodeIdentifier.txt"))
-        val parser = Parser(lexer, mapOf(
-            TokenType.LET to VariableDeclarationStatementCommand(),
-            TokenType.PRINT to PrintStatementCommand(),
-            TokenType.IDENTIFIER to AssignationCommand()
-        ))
-        var statement: StatementNode
-        while(parser.hasNextAST()){
-            statement = parser.nextStatement()
-            println(statement)
-        }
-    }
-    @Test
-    fun testParse2(){
-        val lexer = LexerFactory().createLexer1_0(readSourceCodeFromFile("testCodeIdentifier.txt"))
-        val parser = Parser(lexer, mapOf(
-            TokenType.LET to VariableDeclarationStatementCommand(),
-            TokenType.PRINT to PrintStatementCommand(),
-            TokenType.IDENTIFIER to AssignationCommand()
-        ))
+        val parser = ParserFactory().createParser1_0(lexer)
         var statement: StatementNode
         while (parser.hasNextAST()) {
             statement = parser.nextStatement()
             println(statement)
-
         }
-
     }
 
+    @Test
+    fun testPrintParsing() {
+        val lexer = LexerFactory().createLexer1_0(readSourceCodeFromFile("testCodeIdentifier.txt"))
+        val parser = ParserFactory().createParser1_0(lexer)
+        val statements = mutableListOf<StatementNode>()
+        var statement: StatementNode
+        while (parser.hasNextAST()) {
+            statement = parser.nextStatement()
+            statements.add(statement)
+        }
+        for (s in statements) {
+            if (s is PrintStatementNode) {
+                println(s)
+            }
+        }
+    }
+
+    @Test
+    fun testVariableDeclarationParsing() {
+        val lexer = LexerFactory().createLexer1_0(readSourceCodeFromFile("testCodeIdentifier.txt"))
+        val parser = ParserFactory().createParser1_0(lexer)
+        val statements = mutableListOf<StatementNode>()
+        var statement: StatementNode
+        while (parser.hasNextAST()) {
+            statement = parser.nextStatement()
+            statements.add(statement)
+        }
+        for (s in statements) {
+            if (s is VariableDeclarationNode) {
+                println(s)
+            }
+        }
+    }
+
+    @Test
+    fun testAssignationParsing() {
+        val lexer = LexerFactory().createLexer1_0(readSourceCodeFromFile("testCodeIdentifier.txt"))
+        val parser = ParserFactory().createParser1_0(lexer)
+        val statements = mutableListOf<StatementNode>()
+        var statement: StatementNode
+        while (parser.hasNextAST()) {
+            statement = parser.nextStatement()
+            statements.add(statement)
+        }
+        for (s in statements) {
+            if (s is AssignationNode) {
+                println(s)
+            }
+        }
+    }
 }
