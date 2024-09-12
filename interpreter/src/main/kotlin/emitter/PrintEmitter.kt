3411c8b250dbbc84496c123366707f0f91c2b48e
Your GitHub Username <your.email@example.com>
2024-09-12 02:21:42 -0700

    ciclon se va a la B

--- interpreter/src/main/kotlin/emitter/PrintEmitter.kt
+++ interpreter/src/main/kotlin/emitter/PrintEmitter.kt
@@ -4,6 +4,13 @@ open class PrintEmitter {
     private var count = 0
 
     open fun print(value: Any) {
+        if (value is Double) {
+            if (value % 1 == 0.0) {
+                print(value.toInt())
+                count++
+                return
+            }
+        }
         println(value)
         count++
     }
