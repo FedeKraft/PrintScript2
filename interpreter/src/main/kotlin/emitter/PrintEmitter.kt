8314ac3793521e83a1c83d308a752c624060a4ce
Federico <federicojkraft@gmail.com>
2024-09-12 05:00:07 -0300

    ghfytdcutdcutcfugtfc2

--- interpreter/src/main/kotlin/emitter/PrintEmitter.kt
+++ interpreter/src/main/kotlin/emitter/PrintEmitter.kt
@@ -1,12 +1,14 @@
 package emitter
 
-class PrintEmitter {
+open class PrintEmitter {
     private var count = 0
-    fun print(value: Any) {
+
+    open fun print(value: Any) {
         println(value)
         count++
     }
-    fun getCount(): Int {
+
+    open fun getCount(): Int {
         return count
     }
 }
