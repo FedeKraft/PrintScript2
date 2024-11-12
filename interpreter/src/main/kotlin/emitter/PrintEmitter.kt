efafa7aefa0c9b3f4a101cf26c2d915bff5a6c5c
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-11-12 12:47:45 -0300

    cambio printEmitter y de version

--- interpreter/src/main/kotlin/emitter/PrintEmitter.kt
+++ interpreter/src/main/kotlin/emitter/PrintEmitter.kt
@@ -2,18 +2,21 @@ package emitter
 
 open class PrintEmitter {
     private var count = 0
+    private val output = mutableListOf<String>()
 
-    open fun print(value: Any) {
-        if (value is Double) {
-            if (value % 1 == 0.0) {
-                print(value.toInt())
-                count++
-                return
-            }
+    open fun print(value: Any): String {
+        val outputValue = if (value is Double && value % 1 == 0.0) {
+            value.toInt().toString()
+        } else {
+            value.toString()
         }
-        println(value)
+        output.add(outputValue)
+        println(outputValue)
         count++
+        return outputValue
     }
 
     open fun getCount(): Int = count
+
+    fun getOutput(): String = output.joinToString(separator = "\n")
 }
